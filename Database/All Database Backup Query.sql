0922b6481fff8ded02c5e006a27f67f45461fda8
Manoj Mahato <mmahato@braindigit.com>
2016-07-18 12:47:25 +0545

    All db backup

--- Database/All Database Backup Query.sql
+++ Database/All Database Backup Query.sql
@@ -13,8 +13,8 @@ WHILE @ID <= (SELECT MAX(ID) FROM @DatabaseName)
 BEGIN
 
 declare @DatabseNAme nvarchar(256)
-declare @DatabseNAmeMessage nvarchar(500)
 select @DatabseNAme = name from @DatabaseName where id = @id
+declare @DatabseNAmeMessage nvarchar(500) = 'Full Backup '+ @DatabseNAme + ' at '+ cast(getdate()  as varchar(256))
 declare @Path  nvarchar(256) = @Location +  @DatabseNAme +'.bak'
 BACKUP DATABASE @DatabseNAme
 TO DISK = @Path
